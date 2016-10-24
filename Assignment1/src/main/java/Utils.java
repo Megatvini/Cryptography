e6c8989dc5caf7124186313060982e96be3b0a7b
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2016-10-24 17:16:16 +0400

    Refactor code to classes

---
+++ Assignment1/src/main/java/Utils.java
@@ -0,0 +1,70 @@
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Utils {
+    public static String hexToBase64(String hexString) throws DecoderException {
+        byte[] bytes = Hex.decodeHex(hexString.toCharArray());
+        return Base64.encodeBase64String(bytes);
+    }
+
+    public static String xorHexStrings(String one, String two) throws DecoderException {
+        if (one.length() != two.length()) {
+            throw new DecoderException("Strings must be equal length");
+        }
+
+        byte[] bytes1 = Hex.decodeHex(one.toCharArray());
+        byte[] bytes2 = Hex.decodeHex(two.toCharArray());
+
+        byte[] xorRes = new byte[bytes1.length];
+        for (int i=0; i<xorRes.length; i++) {
+            xorRes[i] = (byte) (bytes1[i] ^ bytes2[i]);
+        }
+
+        char[] chars = Hex.encodeHex(xorRes);
+        return new String(chars);
+    }
+
+    public static String decodeSingleByteXor(String hexString) throws DecoderException {
+        byte[] bytes = Hex.decodeHex(hexString.toCharArray());
+        List<String> candidates = generateCandidates(bytes);
+        return getBestCandidate(candidates);
+    }
+
+    private static Integer scoreEnglishText(String text) {
+        int resScore = 0;
+        for (int i=0; i<text.length(); i++) {
+            char c = text.charAt(i);
+            if (Character.isLetter(c) || Character.isSpaceChar(c))
+                resScore++;
+        }
+        return resScore;
+    }
+
+    private static String getBestCandidate(List<String> candidates) {
+        Collections.sort(candidates, (o1, o2) -> scoreEnglishText(o1).compareTo(scoreEnglishText(o2)));
+        Collections.reverse(candidates);
+        return candidates.get(0);
+    }
+
+    private static List<String> generateCandidates(byte[] bytes) {
+        List<String> res = new ArrayList<>();
+        for (byte b = 0; b < Byte.MAX_VALUE; b++) {
+            byte[] copy = cloneByteArrayXor(bytes, b);
+            res.add(new String(copy));
+        }
+        return res;
+    }
+
+    private static byte[] cloneByteArrayXor(byte[] bytes, byte byteToXor) {
+        byte[] res = new byte[bytes.length];
+        for (int i=0; i<res.length; i++) {
+            res[i] = (byte) (bytes[i] ^ byteToXor);
+        }
+        return res;
+    }
+}
--- Assignment1/src/test/java/UtilsTest.java
+++ Assignment1/src/test/java/UtilsTest.java
@@ -1,18 +1,13 @@
 import org.apache.commons.codec.DecoderException;
 import org.junit.Test;
 
-import java.io.UnsupportedEncodingException;
-
 import static org.junit.Assert.*;
 
-/**
- * Created by Nika Doghonadze
- */
-public class MainTest {
+public class UtilsTest {
     @Test
     public void testHexToBase64() throws DecoderException {
         String hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-        String res = Main.hexToBase64(hexString);
+        String res = Utils.hexToBase64(hexString);
         String expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
         assertTrue(res.equals(expected));
     }
@@ -22,7 +17,7 @@ public class MainTest {
         String one = "1c0111001f010100061a024b53535009181c";
         String two = "686974207468652062756c6c277320657965";
         String expected = "746865206b696420646f6e277420706c6179";
-        String res = Main.xorHexStrings(one, two);
+        String res = Utils.xorHexStrings(one, two);
         assertTrue(expected.equals(res));
     }
 
@@ -30,7 +25,7 @@ public class MainTest {
     public void testDecodeSingleByteXor() throws DecoderException {
         String cipher = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
         String expected = "Cooking MC's like a pound of bacon";
-        String res = Main.decodeSingleByteXor(cipher);
+        String res = Utils.decodeSingleByteXor(cipher);
         assertTrue(res.equals(expected));
     }
 
