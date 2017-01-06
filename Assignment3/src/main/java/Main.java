d436c89a4966c9004c3174fab61bd830a4935a43
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2017-01-06 16:10:43 +0400

    Add main class to run all tasks at once

---
+++ Assignment3/src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Nika Doghonadze
+ */
+public class Main {
+    public static void main(String[] args) throws Exception{
+        CBCPaddingOracle.main(args);
+        CTR.main(args);
+        BreakCTRWithSubstitutions.main(args);
+        BreakRepeatingKeyXOR.main(args);
+        MT_RNG.main(args);
+        FindRNGSeed.main(args);
+        CloneMT_RNG.main(args);
+        RNGCipher.main(args);
+    }
+}
